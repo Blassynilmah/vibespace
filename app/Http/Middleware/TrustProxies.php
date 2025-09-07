e8926af4533fea46bfe05c15b008731f2e93026b
Blassy <blassynilmah@gmail.com>
2025-09-07 15:33:16 -0400

    Fix: Trust all proxies for HTTPS asset URLs on Render

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,33 +3,21 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
-     * You can set this in .env with TRUSTED_PROXIES=* 
-     * to trust all proxies (like Render/Heroku).
+     * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // Trust all proxies
 
     /**
      * The headers that should be used to detect proxies.
      *
-     * This tells Laravel to respect X-Forwarded-* headers.
+     * @var int
      */
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
-
-    public function __construct()
-    {
-        // Load proxies from env, defaults to trusting all (*)
-        $this->proxies = env('TRUSTED_PROXIES', '*');
-    }
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
